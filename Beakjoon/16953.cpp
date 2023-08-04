380eb13e37a2997e1905e86528033c5af4ea9a77
minky <minkyeong02@daum.net>
2023-08-04 20:54:21 +0900

    A->B

---
+++ Beakjoon/16953.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long A, B;
+long check = 0;
+
+void DFS(long depth, long a) {
+	if (B == a) {
+		cout << depth;
+		check = 1;
+		return;
+	}
+	if (a > B) return;
+	DFS(depth + 1, 2 * a);
+	DFS(depth + 1, a * 10 + 1);
+}
+
+
+int main() {
+	cin >> A >> B;
+	DFS(1, A);
+	if (check == 0) cout << -1;
+}
