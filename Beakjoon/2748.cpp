aa79c258e88881405b5888722bb47d0812315885
minky <minkyeong02@daum.net>
2023-08-22 17:32:57 +0900

    피보나치 수 2

---
+++ Beakjoon/2748.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long * fibo = new long long[90];
+	int num;
+	cin >> num;
+
+	fibo[0] = 0;
+	fibo[1] = 1;
+
+	for (int i = 2; i < num+1; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	}
+
+	cout << fibo[num];
+}
